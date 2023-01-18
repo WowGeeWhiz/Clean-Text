159cd8fe4a56413e08523a47dc35ab180cf11960
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2023-01-18 11:40:59 -0500

    Create Project
    Upload the program

---
+++ Clean Text/Clean Text/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Clean_Text
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entryTextBox = new System.Windows.Forms.TextBox();
+            this.entryLabel = new System.Windows.Forms.Label();
+            this.headLabel = new System.Windows.Forms.Label();
+            this.removeLabel = new System.Windows.Forms.Label();
+            this.removeTextBox = new System.Windows.Forms.TextBox();
+            this.replaceTextButton = new System.Windows.Forms.CheckBox();
+            this.replaceTextBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // entryTextBox
+            // 
+            this.entryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entryTextBox.Location = new System.Drawing.Point(12, 57);
+            this.entryTextBox.Multiline = true;
+            this.entryTextBox.Name = "entryTextBox";
+            this.entryTextBox.PlaceholderText = "Enter original text here";
+            this.entryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.entryTextBox.Size = new System.Drawing.Size(381, 108);
+            this.entryTextBox.TabIndex = 0;
+            this.entryTextBox.TextChanged += new System.EventHandler(this.entryTextBox_TextChanged);
+            // 
+            // entryLabel
+            // 
+            this.entryLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entryLabel.AutoSize = true;
+            this.entryLabel.Location = new System.Drawing.Point(12, 39);
+            this.entryLabel.Name = "entryLabel";
+            this.entryLabel.Size = new System.Drawing.Size(76, 15);
+            this.entryLabel.TabIndex = 1;
+            this.entryLabel.Text = "Original Text:";
+            // 
+            // headLabel
+            // 
+            this.headLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headLabel.AutoSize = true;
+            this.headLabel.Location = new System.Drawing.Point(105, 9);
+            this.headLabel.Name = "headLabel";
+            this.headLabel.Size = new System.Drawing.Size(191, 15);
+            this.headLabel.TabIndex = 2;
+            this.headLabel.Text = "Ethan Johnson\'s Text Cleaning Tool";
+            // 
+            // removeLabel
+            // 
+            this.removeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeLabel.AutoSize = true;
+            this.removeLabel.Location = new System.Drawing.Point(12, 182);
+            this.removeLabel.Name = "removeLabel";
+            this.removeLabel.Size = new System.Drawing.Size(91, 15);
+            this.removeLabel.TabIndex = 4;
+            this.removeLabel.Text = "Text to Remove:";
+            // 
+            // removeTextBox
+            // 
+            this.removeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeTextBox.Location = new System.Drawing.Point(12, 200);
+            this.removeTextBox.Multiline = true;
+            this.removeTextBox.Name = "removeTextBox";
+            this.removeTextBox.PlaceholderText = "Enter text you want to remove";
+            this.removeTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.removeTextBox.Size = new System.Drawing.Size(381, 44);
+            this.removeTextBox.TabIndex = 3;
+            this.removeTextBox.TextChanged += new System.EventHandler(this.removeTextBox_TextChanged);
+            // 
+            // replaceTextButton
+            // 
+            this.replaceTextButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.replaceTextButton.AutoSize = true;
+            this.replaceTextButton.Location = new System.Drawing.Point(12, 262);
+            this.replaceTextButton.Name = "replaceTextButton";
+            this.replaceTextButton.Size = new System.Drawing.Size(94, 19);
+            this.replaceTextButton.TabIndex = 5;
+            this.replaceTextButton.Text = "Replace Text:";
+            this.replaceTextButton.UseVisualStyleBackColor = true;
+            this.replaceTextButton.CheckedChanged += new System.EventHandler(this.replaceTextButton_CheckedChanged);
+            // 
+            // replaceTextBox
+            // 
+            this.replaceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.replaceTextBox.Location = new System.Drawing.Point(12, 287);
+            this.replaceTextBox.Multiline = true;
+            this.replaceTextBox.Name = "replaceTextBox";
+            this.replaceTextBox.PlaceholderText = "Enter text you want to replace the removed text with";
+            this.replaceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.replaceTextBox.Size = new System.Drawing.Size(381, 44);
+            this.replaceTextBox.TabIndex = 6;
+            this.replaceTextBox.TextChanged += new System.EventHandler(this.replaceTextBox_TextChanged);
+            // 
+            // runButton
+            // 
+            this.runButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runButton.Location = new System.Drawing.Point(317, 416);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 7;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(93, 416);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 8;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(174, 416);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Reset Form";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(12, 416);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 10;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 451);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.replaceTextBox);
+            this.Controls.Add(this.replaceTextButton);
+            this.Controls.Add(this.removeLabel);
+            this.Controls.Add(this.removeTextBox);
+            this.Controls.Add(this.headLabel);
+            this.Controls.Add(this.entryLabel);
+            this.Controls.Add(this.entryTextBox);
+            this.MinimumSize = new System.Drawing.Size(420, 490);
+            this.Name = "Form1";
+            this.Text = "Text Cleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox entryTextBox;
+        private Label entryLabel;
+        private Label headLabel;
+        private Label removeLabel;
+        private TextBox removeTextBox;
+        private CheckBox replaceTextButton;
+        private TextBox replaceTextBox;
+        private Button runButton;
+        private Button helpButton;
+        private Button resetButton;
+        private Button closeButton;
+    }
+}
---
+++ Clean Text/Clean Text/Form1.cs
@@ -0,0 +1,140 @@
+namespace Clean_Text
+{
+    public partial class Form1 : Form
+    {
+        //booleans for use throughout program
+        bool loading, validEntry, validRemove, validReplace;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetForm();
+        }
+
+        //method to reset the form
+        private void ResetForm()
+        {
+            loading = true; //start loading (disable other methods)
+
+            validEntry = validRemove = validReplace = false; //mark entries invalid
+
+            //empty and disable replace text box
+            replaceTextBox.Enabled = false; 
+            replaceTextBox.Text = "";
+
+            replaceTextButton.Checked = false; //uncheck replace text button
+
+            entryTextBox.Text = ""; //empty entry text box
+
+            removeTextBox.Text = ""; //empty remove text box
+
+            runButton.Enabled = false; //disable run button
+
+            loading = false; //stop loading (other methods will run now)
+        }
+
+        //check for valid inputs
+        private void CheckValid()
+        {
+            if (validEntry && validRemove) //if valid entry and remove
+            {
+                if (replaceTextButton.Checked && !validReplace) runButton.Enabled = false; //if replacing and not valid replace disable run button
+                else runButton.Enabled = true; //otherwise enable run button
+            }
+            else runButton.Enabled = false; //disable run button
+        }
+
+        //text in remove text box changed
+        private void removeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (loading) return; //if loading break
+            if (removeTextBox.Text == null || removeTextBox.Text == "") validRemove = false; //if empty, mark as invalid
+            else validRemove = true; //otherwise valid
+            CheckValid(); //check for readiness to run
+        }
+
+        //text in replace text box changed
+        private void replaceTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (loading) return; //if loading break
+            if (replaceTextBox.Text == null || replaceTextBox.Text == "") validReplace = false; //if empty, mark as invalid
+            else validReplace = true; //otherwise valid
+            CheckValid(); //check for readiness to run
+        }
+
+        //display help when button is clicked
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This tool can be used to easily remove duplicate strings from the input text.\n" +
+                "1 - Enter the original text in the first box.\n" +
+                "2 - Enter the text you want removed (Note: The tool will look for complete compies of this text," +
+                " if there are multiple strings you want to remove, you will need to run the tool once for each)\n" +
+                "3 - If you would like to REPLACE the text instead of removing it, check the box and enter the text" +
+                " to replace with.\n" +
+                "4 - Press Run.\n\n" +
+                "Find me on GitHub: https://github.com/wowgeewhiz");
+        }
+
+        //when run button is clicked
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            //assign variables for use
+            string tempOutput = "";
+            string input = entryTextBox.Text;
+            string remove = removeTextBox.Text;
+            string replace = replaceTextBox.Text;
+            bool repeat = true;
+
+            while (repeat) //loop until done
+            {
+                int temp; //temp integer for use
+                if (input.Contains(remove)) //if key is in the input variable
+                {
+                    temp = input.IndexOf(remove); //temp is the index of the key
+                    tempOutput += input.Substring(0, temp); //add section of input before key to output
+                    if (replaceTextButton.Checked) tempOutput += replace; //add replace value if checked
+                }
+                else //if key is not in the remaining input
+                {
+                    tempOutput += input; //add the remaining input to output
+                    break; //break repeat loop
+                }
+                input = input.Substring(temp + remove.Length); //shorten input
+            }
+
+            Clipboard.SetText(tempOutput); //copy output to clipboard
+            MessageBox.Show("\"" + tempOutput + "\" has been copied to your clipboard."); //display output
+            ResetForm(); //reset the form
+        }
+
+        //close the form
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //replace text button is checked/unchecked
+        private void replaceTextButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loading) return; //if loading, break
+            if (replaceTextButton.Checked) replaceTextBox.Enabled = true; //enable text box if checked
+            else replaceTextBox.Enabled = false; //disable text box if checked
+            CheckValid(); //check for readiness to run
+        }
+
+        //reset form
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        //entry text box is changed
+        private void entryTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (loading) return; //break if loading
+            if (entryTextBox.Text == null || entryTextBox.Text == "") validEntry = false; //if empty, mark as invalid
+            else validEntry = true; //otherwise valid
+            CheckValid(); //check for readiness to run
+        }
+    }
+}
